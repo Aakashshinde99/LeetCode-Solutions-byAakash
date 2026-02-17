b28157d012a6678a710bc540ef217af5f1c0a42c
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2026-02-17 06:21:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (100.00%)

--- 0009-palindrome-number/solution.c
+++ 0009-palindrome-number/solution.c
@@ -1,21 +1,14 @@
-#include<stdbool.h>
-
 bool isPalindrome(int x) {
-
-    if(x < 0){
+    if (x < 0) {
         return false;
     }
+    long long num = x;
+    long long rev = 0;
 
-    long long reversed_x = 0;
-    int original_x = x;
-
-    while(x != 0){
-
-        int digit = x % 10;
-        reversed_x = reversed_x * 10 + digit;
-        x /= 10;
+    while (num != 0) {
+        int rem = num % 10;
+        rev = rev * 10 + rem;
+        num /= 10;
     }
-    return (reversed_x == original_x);
+    return rev == x;
 }
-
-
