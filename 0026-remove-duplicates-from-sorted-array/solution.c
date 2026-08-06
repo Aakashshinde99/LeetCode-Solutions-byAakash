2660110d3f974a5d53224947fb7a6196ad805e16
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2026-08-06 14:29:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13 MB (41.94%)

--- 0026-remove-duplicates-from-sorted-array/solution.c
+++ 0026-remove-duplicates-from-sorted-array/solution.c
@@ -1,18 +1,32 @@
 int removeDuplicates(int* nums, int numsSize) {
-    int count = numsSize;
-
-    for (int i = 0; i < count - 1; i++) {
-
-        if (nums[i] == nums[i + 1]) {
-
-            for (int j = i; j < count - 1; j++) {
-                nums[j] = nums[j + 1];
-            }
-
-            count--;
-            i--;
+    
+    int r = 1, l = 0;
+    while(r < numsSize){
+        if(nums[l] == nums[r]){
+            r++;
         }
+        else{
+            nums[++l] = nums[r];
+            
+        }
+        
     }
+    return l+1;
+    
+    // int count = numsSize;
 
-    return count;
+    // for (int i = 0; i < count - 1; i++) {
+
+    //     if (nums[i] == nums[i + 1]) {
+
+    //         for (int j = i; j < count - 1; j++) {
+    //             nums[j] = nums[j + 1];
+    //         }
+
+    //         count--;
+    //         i--;
+    //     }
+    // }
+
+    // return count;
 }
