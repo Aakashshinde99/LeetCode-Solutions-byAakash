29d82328d0b7deb6ae45f777a7afc1384698dde0
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2026-08-16 16:21:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (81.79%)

---
+++ 0792-binary-search/solution.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target) {
+    int low = 0, mid = 0, high = numsSize-1;
+    while(low <= high){
+        mid = (low + high) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(target > nums[mid]){
+            low = mid+1;
+        }
+        else if(target < nums[mid]){
+            high = mid-1;
+        }
+    }
+    return -1;
+}
